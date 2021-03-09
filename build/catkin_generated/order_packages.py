b1d3e156ee3e23d7f7fdb09006b3899ad99867b3
Ivan5d <ivan.d557d@gmail.com>
2021-03-08 23:31:44 -0600

    .

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ivan5d/turtlebots/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ivan5d/turtlebots/devel;/home/ivan5d/catkin_ws/devel;/home/ivan5d/vision_sim_ws/devel;/opt/ros/kinetic'.split(';') if '/home/ivan5d/turtlebots/devel;/home/ivan5d/catkin_ws/devel;/home/ivan5d/vision_sim_ws/devel;/opt/ros/kinetic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PKG_CONFIG_PATH='/home/ivan5d/catkin_ws/devel/lib/pkgconfig:/home/ivan5d/vision_sim_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig'
+export PWD='/home/ivan5d/turtlebots/build'
