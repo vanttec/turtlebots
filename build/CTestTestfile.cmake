0e78655735f3da89a3e056a1e63e5dbbc4e65685
Ivan5d <ivan.d557d@gmail.com>
2021-03-28 00:24:17 -0600

    build uploaded

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -6,10 +6,5 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs(gtest)
 subdirs(vanttec_uv_sim/uv_worlds)
-subdirs(vanttec_uv_sim/sensors/velodyne_simulator/velodyne_description)
-subdirs(vanttec_uv_sim/sensors/velodyne_simulator/velodyne_simulator)
 subdirs(vanttec_uv_sim/vtec_s3_description)
 subdirs(alex2)
-subdirs(vanttec_uv_sim/sensors/realsense_gazebo_plugin)
-subdirs(vanttec_uv_sim/sensors/velodyne_simulator/velodyne_gazebo_plugins)
-subdirs(vanttec_uv_sim/sensors/sensor_plugins)
