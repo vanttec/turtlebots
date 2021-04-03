2e2c1bf0832cb6ac6bb71fa68146bd1f7270bec9
Ivan5d <ivan.d557d@gmail.com>
2021-04-03 12:57:27 -0600

    mv javi

--- javi/build/javi/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xaviar551/Desktop/turtlebots/javi/src/javi
-# Build directory: /home/xaviar551/Desktop/turtlebots/javi/build/javi
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
