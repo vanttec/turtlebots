00d3dad7a72947ce0a1c1583e9b2a8725153fc34
Ivan5d <ivan.d557d@gmail.com>
2021-04-03 12:56:17 -0600

    devel uploaded

--- devel/share/alex2/cmake/alex2Config.cmake
+++ devel/share/alex2/cmake/alex2Config.cmake
@@ -67,7 +67,7 @@ set(alex2_CONFIG_INCLUDED TRUE)
 
 # set variables for source/devel/install prefixes
 if("TRUE" STREQUAL "TRUE")
-  set(alex2_SOURCE_PREFIX /home/ivan5d/turtlebots/src/alex2)
+  set(alex2_SOURCE_PREFIX /home/ivan5d/turtlebots/src/turtlebots_ws/alex2)
   set(alex2_DEVEL_PREFIX /home/ivan5d/turtlebots/devel)
   set(alex2_INSTALL_PREFIX "")
   set(alex2_PREFIX ${alex2_DEVEL_PREFIX})
@@ -110,7 +110,7 @@ if(NOT " " STREQUAL " ")
         message(FATAL_ERROR "Project 'alex2' specifies '${idir}' as an include dir, which is not found.  It does not exist in '${include}'.  ${_report}")
       endif()
     else()
-      message(FATAL_ERROR "Project 'alex2' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/ivan5d/turtlebots/src/alex2/${idir}'.  ${_report}")
+      message(FATAL_ERROR "Project 'alex2' specifies '${idir}' as an include dir, which is not found.  It does neither exist as an absolute directory nor in '/home/ivan5d/turtlebots/src/turtlebots_ws/alex2/${idir}'.  ${_report}")
     endif()
     _list_append_unique(alex2_INCLUDE_DIRS ${include})
   endforeach()
@@ -154,7 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-    foreach(path /home/ivan5d/turtlebots/devel/lib;/home/ivan5d/catkin_ws/devel/lib;/home/ivan5d/turtlebots/devel/lib;/home/ivan5d/vision_sim_ws/devel/lib;/opt/ros/kinetic/lib)
+    foreach(path /home/ivan5d/turtlebots/devel/lib;/home/ivan5d/turtlebots/devel/lib;/home/ivan5d/vision_sim_ws/devel/lib;/opt/ros/kinetic/lib)
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
