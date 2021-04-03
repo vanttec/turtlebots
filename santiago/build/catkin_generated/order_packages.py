3b81f038bde34abc0b9eb41b8240e3aecace0068
Ivan5d <ivan.d557d@gmail.com>
2021-04-03 12:58:09 -0600

    Deleted santiago

--- santiago/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/santiago/Desktop/turtlebots/santiago/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/santiago/Desktop/turtlebots/santiago/devel;/home/santiago/Desktop/turtlebots/devel;/opt/ros/melodic'.split(';') if '/home/santiago/Desktop/turtlebots/santiago/devel;/home/santiago/Desktop/turtlebots/devel;/opt/ros/melodic' != '' else []
--- santiago/build/santiago/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/santiago/Desktop/turtlebots/santiago/src/santiago
-# Build directory: /home/santiago/Desktop/turtlebots/santiago/build/santiago
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
