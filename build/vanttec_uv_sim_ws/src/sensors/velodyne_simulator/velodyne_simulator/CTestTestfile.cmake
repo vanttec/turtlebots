4f82e69bab0f6c39ba8c4dffb338bacfe5205454
Ivan5d <ivan.d557d@gmail.com>
2021-05-06 00:29:02 -0500

    changes

---
+++ build/vanttec_uv_sim_ws/src/sensors/velodyne_simulator/velodyne_simulator/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ivan5d/turtlebots/src/vanttec_uv_sim_ws/src/sensors/velodyne_simulator/velodyne_simulator
+# Build directory: /home/ivan5d/turtlebots/build/vanttec_uv_sim_ws/src/sensors/velodyne_simulator/velodyne_simulator
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ build/vanttec_uv_sim_ws/src/vtec_s3_description/cmake_install.cmake
@@ -0,0 +1,49 @@
+# Install script for directory: /home/ivan5d/turtlebots/src/vanttec_uv_sim_ws/src/vtec_s3_description
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/home/ivan5d/turtlebots/install")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/lib/pkgconfig" TYPE FILE FILES "/home/ivan5d/turtlebots/build/vanttec_uv_sim_ws/src/vtec_s3_description/catkin_generated/installspace/vtec_s3_description.pc")
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/vtec_s3_description/cmake" TYPE FILE FILES
+    "/home/ivan5d/turtlebots/build/vanttec_uv_sim_ws/src/vtec_s3_description/catkin_generated/installspace/vtec_s3_descriptionConfig.cmake"
+    "/home/ivan5d/turtlebots/build/vanttec_uv_sim_ws/src/vtec_s3_description/catkin_generated/installspace/vtec_s3_descriptionConfig-version.cmake"
+    )
+endif()
+
+if(NOT CMAKE_INSTALL_COMPONENT OR "${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified")
+  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/vtec_s3_description" TYPE FILE FILES "/home/ivan5d/turtlebots/src/vanttec_uv_sim_ws/src/vtec_s3_description/package.xml")
+endif()
+
