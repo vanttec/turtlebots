c25524e25b420e328e6a27478b95909ebaea07c5
Ivan5d <ivan.d557d@gmail.com>
2021-05-05 21:20:48 -0500

    Changes

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,10 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/ivan5d/turtlebots/devel:$CMAKE_PREFIX_PATH"
-export LD_LIBRARY_PATH="/home/ivan5d/turtlebots/devel/lib:$LD_LIBRARY_PATH"
-export PKG_CONFIG_PATH="/home/ivan5d/turtlebots/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/ivan5d/turtlebots/build'
-export PYTHONPATH="/home/ivan5d/turtlebots/devel/lib/python2.7/dist-packages:$PYTHONPATH"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/ivan5d/turtlebots/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
-export ROS_PACKAGE_PATH="/home/ivan5d/turtlebots/src:$ROS_PACKAGE_PATH"
--- build/catkin_generated/stamps/Project/_setup_util.py.stamp
+++ build/catkin_generated/stamps/Project/_setup_util.py.stamp
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/ivan5d/vanttec_uv_sim_ws/devel;/opt/ros/kinetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/ivan5d/turtlebots/devel;/home/ivan5d/vanttec_uv_sim_ws/devel;/opt/ros/kinetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
